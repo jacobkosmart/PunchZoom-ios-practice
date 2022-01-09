4209af04371a1c59e7411944e0de80ff789f83f2
jacobkosmart <jacobkosmart@gmail.com>
2022-01-10 08:59:29 +0900

    Initial Commit

---
+++ PinchZoom-App/PinchZoom_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchZoom_AppApp.swift
+//  PinchZoom-App
+//
+//  Created by Jacob Ko on 2022/01/10.
+//
+
+import SwiftUI
+
+@main
+struct PinchZoom_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
